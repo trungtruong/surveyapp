a4a7587cd9bf663d87d6ee4851884635417b4be2
quangtranvan <quang.tranvan@harveynash.vn>
2013-08-08 16:37:08 +0700

    update survey model

--- src/main/java/com/harveynash/surveyapp/model/Survey.java
+++ src/main/java/com/harveynash/surveyapp/model/Survey.java
@@ -1,7 +1,36 @@
 package com.harveynash.surveyapp.model;
 
+import java.sql.Date;
+
 
 public class Survey {
-    int id;
-    StringBuffer name;
+    private Integer surveyId;
+    private String surveyName;
+    private Date startDate;
+    private Date endDate;
+	public Integer getSurveyId() {
+		return surveyId;
+	}
+	public void setSurveyId(Integer surveyId) {
+		this.surveyId = surveyId;
+	}
+	public String getSurveyName() {
+		return surveyName;
+	}
+	public void setSurveyName(String surveyName) {
+		this.surveyName = surveyName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+    
 }
