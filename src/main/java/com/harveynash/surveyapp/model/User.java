d54442936d9c0324f5a4b603d3f29679aa2d3805
quangtranvan <quang.tranvan@harveynash.vn>
2013-08-08 16:30:10 +0700

    add user model

--- src/main/java/com/harveynash/surveyapp/model/User.java
+++ src/main/java/com/harveynash/surveyapp/model/User.java
@@ -1,6 +1,49 @@
 package com.harveynash.surveyapp.model;
 
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.NONE)
 public class User {
+	@XmlTransient
+	int userId;
+	@XmlElement
+	String email;
+	@XmlElement
+	String firstName;
+	@XmlElement
+	String lastName;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 
 }
