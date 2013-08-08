b79c28d46eb864c77327f62fc8ecf306c80d50c1
Lê Phú Hải <haiphule@gmail.com>
2013-08-08 16:42:09 +0700

    updating survey model

--- src/main/java/com/harveynash/surveyapp/model/Survey.java
+++ src/main/java/com/harveynash/surveyapp/model/Survey.java
@@ -1,7 +1,63 @@
 package com.harveynash.surveyapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlRootElement (name = "survey")
+@XmlAccessorType(XmlAccessType.NONE)
 public class Survey {
-    int id;
-    StringBuffer name;
+    
+    @XmlTransient
+    private int surveyId;
+    
+    @XmlElement
+    private String surveyName;
+    
+    @XmlElement
+    private List<Question> questionList;
+    private Date startDate;
+    private Date endDate;
+    
+    public Integer getSurveyId() {
+        return surveyId;
+    }
+    public void setSurveyId(Integer surveyId) {
+        this.surveyId = surveyId;
+    
+    public String getSurveyName() {
+        return surveyName;
+    }
+    public void setSurveyName(String surveyName) {
+        this.surveyName = surveyName;
+    
+    public List<Question> getQuestionList() {
+    
+        return questionList;
+    }
+    
+    public void setQuestionList(List<Question> questionList) {
+    
+    this.questionList = questionList;
+    public Date getStartDate() {
+        return startDate;
+    }
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+    public Date getEndDate() {
+        return endDate;
+    }
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Survey() {
+        this.questionList = new ArrayList<Question>();
+    }
 }
