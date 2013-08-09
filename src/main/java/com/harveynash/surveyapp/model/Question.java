d2b2ee8447c79c7b25dd25725989e0f2d011a07c
Lê Phú Hải <haiphule@gmail.com>
2013-08-09 15:45:27 +0700

    upfile homecontroller, question, survey

--- src/main/java/com/harveynash/surveyapp/model/Question.java
+++ src/main/java/com/harveynash/surveyapp/model/Question.java
@@ -1,61 +1,65 @@
 package com.harveynash.surveyapp.model;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
+
 import javax.xml.bind.annotation.XmlTransient;
 
-@XmlRootElement(name = "question")
-@XmlAccessorType(XmlAccessType.NONE)
 public class Question {
     
     @XmlTransient
     private int questionId;
     
-    @XmlElement
     private String questionText;
     
-    @XmlElement
-    private QuestionType questionType = QuestionType.TEXT;
+    private QuestionType questionType;
     
-    @XmlElement
     private QuestionOption questionOption = null;
+
     
     public int getQuestionId() {
     
         return questionId;
     }
 
+    
     public void setQuestionId(int questionId) {
     
         this.questionId = questionId;
     }
 
-    public void setText(String text) {
-        this.questionText = text;
-    }
     
-    public String getText () {
+    public String getQuestionText() {
+    
         return questionText;
     }
-    
-    public void setQuestionType(QuestionType type) {
 
-        this.questionType = type;
+    
+    public void setQuestionText(String questionText) {
+    
+        this.questionText = questionText;
     }
+
     
     public QuestionType getQuestionType() {
-
+    
         return questionType;
     }
+
     
-    public void setQuestionOption(QuestionOption option) {
-
-        this.questionOption = option;
+    public void setQuestionType(QuestionType questionType) {
+    
+        this.questionType = questionType;
     }
-    public QuestionOption getQuestionOption() {
 
+    
+    public QuestionOption getQuestionOption() {
+    
         return questionOption;
     }
+
+    
+    public void setQuestionOption(QuestionOption questionOption) {
+    
+        this.questionOption = questionOption;
+    }
+
 }
