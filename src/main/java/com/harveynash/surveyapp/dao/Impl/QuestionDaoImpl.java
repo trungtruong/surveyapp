24426c8a56edf25dc42e38c0dc0df57564df4d74
quangtranvan <quang.tranvan@harveynash.vn>
2013-08-12 15:35:32 +0700

    Implement DAO

--- src/main/java/com/harveynash/surveyapp/dao/Impl/QuestionDaoImpl.java
+++ src/main/java/com/harveynash/surveyapp/dao/Impl/QuestionDaoImpl.java
@@ -4,49 +4,66 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.harveynash.surveyapp.dao.QuestionDao;
+import com.harveynash.surveyapp.mapper.QuestionMapper;
+import com.harveynash.surveyapp.mapper.UserMapper;
 import com.harveynash.surveyapp.model.Question;
 import com.harveynash.surveyapp.model.QuestionOption;
 import com.harveynash.surveyapp.model.QuestionType;
+import com.harveynash.surveyapp.model.User;
 
 
 public class QuestionDaoImpl implements QuestionDao{
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplateObject;
 
 	@Override
 	public void setDataSource(DataSource dataSource) {
-		// TODO Auto-generated method stub
+		this.dataSource = dataSource;
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 		
 	}
 
 	@Override
 	public void create(String questionText, QuestionType questionType,
 			QuestionOption questionOption) {
-		// TODO Auto-generated method stub
+		String SQL = "insert into Question (questionText, QuestionType) values (?, ?)";
+		jdbcTemplateObject.update(SQL,questionText,questionType);
+		return;	
 		
 	}
 
 	@Override
 	public Question getQuestion(Integer questionId) {
-		// TODO Auto-generated method stub
-		return null;
+		String SQL = "select * from Question where questionId = ?";
+		Question question = jdbcTemplateObject.queryForObject(SQL, new Object[] {questionId}, new QuestionMapper());
+		return question;
 	}
 
 	@Override
 	public List<Question> listUser() {
-		// TODO Auto-generated method stub
-		return null;
+		String SQL = "select * from Question";
+	    List <Question> questions = jdbcTemplateObject.query(SQL, 
+	                                new QuestionMapper());
+	    return questions;
 	}
 
 	@Override
 	public void delete(Integer questionId) {
-		// TODO Auto-generated method stub
+		String SQL = "delete from Question where questionId = ?";
+	     jdbcTemplateObject.update(SQL, questionId);
+	     return;
 		
 	}
 
 	@Override
 	public void update(Integer questionId, String questionText,
 			QuestionType questionType, QuestionOption questionOption) {
-		// TODO Auto-generated method stub
+		String SQL = "update Question set questionName = ?, questionType = ? where userId = ?";
+	    jdbcTemplateObject.update(SQL, questionText,questionType,questionId);
+	    return;
 		
 	}
 
