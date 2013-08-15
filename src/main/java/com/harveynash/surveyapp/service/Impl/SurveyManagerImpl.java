0d5ff259537b7f58142a7be32b1bf82ac0461694
quangtranvan <quang.tranvan@harveynash.vn>
2013-08-15 10:53:07 +0700

    Update List Survey Detail

--- src/main/java/com/harveynash/surveyapp/service/Impl/SurveyManagerImpl.java
+++ src/main/java/com/harveynash/surveyapp/service/Impl/SurveyManagerImpl.java
@@ -4,18 +4,39 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.harveynash.surveyapp.dao.QuestionDao;
 import com.harveynash.surveyapp.dao.SurveyDao;
+import com.harveynash.surveyapp.dao.UserDao;
+import com.harveynash.surveyapp.model.Question;
 import com.harveynash.surveyapp.model.Survey;
+import com.harveynash.surveyapp.model.User;
 import com.harveynash.surveyapp.service.SurveyManager;
 
 @Service
 public class SurveyManagerImpl implements SurveyManager {
     
     private SurveyDao surveyDao;
+    private UserDao userDao;
+    private QuestionDao questionDao;
 
     @Override
     public List<Survey> getSurveyList() {
         return surveyDao.listSurveys();
     }
+
+	@Override
+	public Survey getSurveyDetail(Integer surveyId) {
+		List<Question> questions;
+		List<User> users;
+		Survey survey;
+		
+		questions = questionDao.listQuestions(surveyId);
+		users = userDao.listUser(surveyId);
+		survey = surveyDao.getSurvey(surveyId);
+		survey.setQuestionList(questions);
+		survey.setUserList(users);
+		
+		return survey;
+	}
 
 }
--- src/main/java/com/harveynash/surveyapp/service/SurveyManager.java
+++ src/main/java/com/harveynash/surveyapp/service/SurveyManager.java
@@ -6,4 +6,5 @@ import com.harveynash.surveyapp.model.Survey;
 
 public interface SurveyManager {
     public List<Survey> getSurveyList();
+    public Survey getSurveyDetail(Integer surveyId);
 }
